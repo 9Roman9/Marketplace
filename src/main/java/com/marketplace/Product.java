2d0233e3c423bd1bfea03480f24ea54c2fa70ac2
Roman Suslov <roman.suslov.d@gmail.com>
2022-07-22 16:52:34 +0300

    create Product class

---
+++ src/main/java/com/marketplace/Product.java
@@ -0,0 +1,33 @@
+package com.marketplace;
+
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+
+    public Product() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return id + " " + name + " " + price;
+    }
+}
